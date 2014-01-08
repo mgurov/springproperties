681a29127891166734739a327d529ee110181575
Mykola Gurov <ngurov@gmail.com>
2014-01-08 09:16:50 +0100

    started with the splitter

---
+++ src/main/java/mgurov/spring/PropertyFileNamesCommaSeparatedSplitter.java
@@ -0,0 +1,14 @@
+package mgurov.spring;
+
+/**
+ * A wrapper around the {@link com.google.common.base.Splitter} that adds convenience features of wrapping the resulting
+ * strings in prefix/suffix and referring to other "prototype" or "template" property files
+ *
+ * TODO: example
+ *
+ */
+public class PropertyFileNamesCommaSeparatedSplitter {
+
+
+
+}
